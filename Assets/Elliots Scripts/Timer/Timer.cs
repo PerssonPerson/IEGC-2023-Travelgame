2d89ef81ac2c9dfa3985da676fc6ca8b3547faba
PerssonPerson <elliot.rg.persson@gmail.com>
2023-04-25 22:06:34 +0200

    Timer Ticket

--- Assets/Elliots Scripts/Timer/Timer.cs
+++ Assets/Elliots Scripts/Timer/Timer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
@@ -16,6 +17,18 @@ public class Timer : MonoBehaviour
 
     static TextMeshProUGUI timerText;
 
+    static RawImage ticketIMG;
+
+    static Color tempColor;
+
+    static bool qOne;
+
+    static bool qTwo;
+
+    static bool qThree;
+
+    static bool qFour;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,8 +37,8 @@ public class Timer : MonoBehaviour
         timer = 60;
 
         timeLimit = 60;
-        
 
+        TicketShow(1);
     }
 
     // Update is called once per frame
@@ -35,6 +48,48 @@ public class Timer : MonoBehaviour
         if (ElliotsMain.roundActive) 
         {
             timer -= Time.deltaTime;
+        }
+
+        
+        if (timer <= timeLimit * 0.75f && !qOne) 
+        {
+
+
+            TicketShow(2);
+            TicketHide(1);
+
+            qOne = true;
+
+
+        }
+
+        if (timer <= timeLimit * 0.5f && !qTwo)
+        {
+
+
+            TicketShow(3);
+            TicketHide(2);
+
+            qTwo = true;
+        }
+
+        if (timer <= timeLimit * 0.25f && !qThree)
+        {
+
+
+            TicketShow(4);
+            TicketHide(3);
+
+            qThree = true;
+        }
+
+        if (timer <= 0 && !qFour)
+        {
+
+            TicketHide(4);
+
+            qFour = true;
+
         }
 
         if (hasEnteredTrain) //in main
@@ -60,9 +115,38 @@ public class Timer : MonoBehaviour
 
         timer = timeLimit;
 
+        qOne = false;
 
+        qTwo = false;
+
+        qThree = false;
+
+        qFour = false;
     }
 
+    static void TicketShow(int ticketNum) 
+    {
+
+        ticketIMG = GameObject.Find("Ticket" + ticketNum).GetComponent<RawImage>();
+
+        tempColor = ticketIMG.color;
+
+        tempColor.a = 1;
+
+        ticketIMG.color = tempColor;
+    }
+
+    static void TicketHide(int ticketNum)
+    {
+
+        ticketIMG = GameObject.Find("Ticket" + ticketNum).GetComponent<RawImage>();
+
+        tempColor = ticketIMG.color;
+
+        tempColor.a = 0;
+
+        ticketIMG.color = tempColor;
+    }
 
     static void ShowTime() 
     {
